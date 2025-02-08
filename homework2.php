8e6524222f1ef523cf1fb4424adc2d6344bde03f
dnicker1 <dan.kenwill.nick@gmail.com>
2025-02-08 11:45:19 -0500

    Feat - Added HTML boilerplate and an if statement chain that sends a message dependent on the current server time

--- homework2.php
+++ homework2.php
@@ -1,3 +1,33 @@
-<?php
-    
-?>
+<!DOCTYPE html>
+<!--Daniel Nickerson, CIS 215,-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width-device-width, initial-scale=1.0">
+		<link href="Styles/style.css" rel="stylesheet" type="text/css">
+		<title></title>
+	</head>
+		<body>
+			<header>
+
+			</header>
+
+				<section>
+					<?php
+						if (date("H:i") >= "5:00" && date("H:i") < "09:59") {
+							echo "Good Morning!";
+						} elseif (date("H:i") >= "10:00" && date("H:i") < "15:59") {
+							echo "Good Afternoon!";
+						} elseif (date("H:i") >= "16:00" && date("H:i") < "22:00") {
+							echo "Good Night!";
+						} else {
+							echo "Sleep Tight!";
+						}
+					?>
+				</section>
+			<footer>
+
+			</footer>
+		</body>
+
+</html>
